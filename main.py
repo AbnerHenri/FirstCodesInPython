fc5465ddc589beb018ec54becf8f9556c77cb662
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-09-16 20:46:18 -0300

    Light Calc

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-frase = input('Digite uma frase : ')
-recorte = len(frase) // 2
+print('R - Residências')
+print('C - Comércio')
+print('I - Indústria')
 
-frase2 = frase[:recorte]
+instalation_type = input('Qual é o seu tipo de instalação? : ')
+kWh = int(input('Qual a quantidade do kWh? : '))
 
-print('A frase recortada fica : {}' .format(frase2))
-print('Os ultimos caracteres são : {}' .format(frase2[-2:]))
+if(instalation_type == 'R'):
+    if(kWh <= 500):
+        calc = kWh * 0.40
+        print('O valor da sua conta é {:.2f}' .format(calc))
+    else:
+        calc = kWh * 0.65
+        print('O valor da sua conta é {:.2f}'.format(calc))
+elif(instalation_type == 'C'):
+    if(kWh <= 100):
+        calc = kWh * 0.55
+        print('O valor da sua conta é {:.2f}'.format(calc))
+    else:
+        calc = kWh * 0.60
+        print('O valor da sua conta é {:.2f}'.format(calc))
+elif(instalation_type == 'I'):
+    if(kWh <= 5000):
+        calc = kWh * 0.55
+        print('O valor da sua conta é {:.2f}'.format(calc))
+    else:
+        calc = kWh * 0.60
+        print('O valor da sua conta é {:.2f}'.format(calc))
+else:
+    print('O tipo de instalação é inválido')
 
