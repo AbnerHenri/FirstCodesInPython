795a2c6ec3681a45488d1d12a1c5609e0c966784
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-09-21 21:02:30 -0300

    Funciont Challenge

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def border(nome):
-    print('+----------+')
-    print('   ' + nome)
-    print('+----------+')
 
+def verifyString(palavra_tamanho):
+    if palavra_tamanho > 10 and palavra_tamanho < 20:
+        print('O tamanho da palavra está conforme o esperado')
+    else:
+        print('O tamanho da palavra não é aceitavel')
 
-border('Abner')
-border('Joao')
-border('Haik')
-border('Adilson')
+palavra = input('Digite uma palavra entre 10 e 20 caracteres : ')
+palavra_tam = len(palavra)
+
+verifyString(palavra_tam)
