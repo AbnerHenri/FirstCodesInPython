98aa3e340e411715c922f9d86f06a139171821cf
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-09-21 21:11:12 -0300

    String Challenge

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 
-def verifyString(palavra_tamanho):
-    if palavra_tamanho > 10 and palavra_tamanho < 20:
+def verifyString(palavra_tamanho, max, min):
+    if palavra_tamanho > min and palavra_tamanho < max:
         print('O tamanho da palavra está conforme o esperado')
     else:
         print('O tamanho da palavra não é aceitavel')
 
-palavra = input('Digite uma palavra entre 10 e 20 caracteres : ')
+
+
+min = int(input('Qual o mínimo de caracteres? : '))
+max = int(input('Qual o máximo de caracteres? : '))
+
+while max < min :
+    max = int(input('O valor máximo deve ser maior que o valor mínimo, digite outro número : '))
+
+
+palavra = input('Digite uma palavra entre {} e {} caracteres : ' .format(min, max))
 palavra_tam = len(palavra)
 
-verifyString(palavra_tam)
+verifyString(palavra_tam, max, min)
