80d16e0df6e5911801a49443492544c649fdb0e2
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-09-22 21:41:06 -0300

    Fatorial Challenge

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 
-def verifyString(palavra_tamanho, max, min):
+def FatorialNumber(num):
     """
-    Verifica se a o tamanho da palavra
-    está entre o número correto ou não
-
-    -> palavra_tamanho : A palavra digitada
-    -> max : Tamanho máximo da String
-    -> min : Tamanho minimo da String
+    A função recebe um número inteiro, após isso
+    a função multiplica o número por todos os seu antecessores
+    até 1
     """
+    for i in range(1, num, 1):
+        num *= i
+    print(num)
 
-    if palavra_tamanho > min and palavra_tamanho < max:
-        print('O tamanho da palavra está conforme o esperado')
-    else:
-        print('O tamanho da palavra não é aceitavel')
+number = int(input('Digite um número para ver o seu fatorial : '))
+FatorialNumber(number)
 
-
-
-
-min = int(input('Qual o mínimo de caracteres? : '))
-max = int(input('Qual o máximo de caracteres? : '))
-
-while max < min :
-    max = int(input('O valor máximo deve ser maior que o valor mínimo, digite outro número : '))
-
-
-palavra = input('Digite uma palavra entre {} e {} caracteres : ' .format(min, max))
-palavra_tam = len(palavra)
-
-verifyString(palavra_tam, max, min)
-
-help(verifyString)
+help(FatorialNumber)
