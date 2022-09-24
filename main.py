d9bfe95ff02f963027f0a0a2dd868a01b3ea442e
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-09-24 14:44:19 -0300

    Products Challenge

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-def Soma(*number):
-    soma = 0
-    print('Tupla : {}'.format(number))
 
-    for i in number:
-        soma += i
-    return soma
+def Add(product, price, quant):
+    try:
+        a = open('Products.txt', 'at')
+    except:
+        print('Houve um erro')
+    else:
+        a.write('Product : {} | Price : R${} | Quantility : {} \r\n' .format(product, price, quant))
+        print('Produto Cadastrado com sucesso')
+    finally:
+        a.close()
+
+def List():
+    try:
+        a = open('Products.txt', 'rt')
+    except:
+        print('Houve um erro')
+    else:
+        print(a.read())
+    finally:
+        a.close()
 
 
-print('Resultado : {}'  .format(Soma(1, 4, 7, 15,28)))
+# Principal Program
+
+while True:
+
+    product = str(input('Cadastre um produto : '))
+    if(product == 'Sair') : break
+    elif(product == 'Listar') : List()
+
+    price = input('Informe o preço : ')
+    if(price == 'Sair') : break
+    elif(price == 'Listar') : List()
+
+    quant = int(input('Informe a quantidade : '))
+    if(quant == 'Sair') : break
+    elif(quant == 'Listar') : List()
+
+    Add(product, price, quant)
+
